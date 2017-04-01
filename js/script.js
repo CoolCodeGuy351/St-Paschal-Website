35d5872ff5b0aa98edf16b168b6b00740ecced4a
Joe Mulick <joemulick@gmail.com>
2017-03-31 17:08:35 -0700

    Still needs additional styling

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-/*
-
-$ ('p').on('click',function(){
-    $('p').fadeOut(500);
+/* NOT WORKING
+$('.nav').on( "hover", function() {
+    $('.nav').css('background-color', '#8C8B84');
 });
 
+WORKING (Kept for template)
+$('#welcome').on( "click", function() {
+  $('#welcome').fadeOut(500).fadeIn(500);
+});
 
-$('#carousel').document(function(){
-  $('carousel').css('margin: auto')
-  
-  });
 */
 
